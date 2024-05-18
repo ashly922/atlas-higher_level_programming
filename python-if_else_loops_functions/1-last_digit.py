dd2bfb203987ae9ac48816dcfd49b1b1abfbb75b
ashly922 <ashly922@yahoo.com>
2024-05-18 12:15:51 -0500

    adjusted print statements

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
-print(f"Last digit of {number} is {last_digit}")
 if last_digit > 5:
-    print(f"and is greater than 5")
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
 if last_digit == 0:
-    print(f"and is 0")
+    print(f"Last digit of {number} is {last_digit} and is 0")
 if last_digit < 6 != 0:
-    print(f"and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
